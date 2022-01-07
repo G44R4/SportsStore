4ddd3e3700babf490ff66d5a511736f753dcc414
Nikoletta Haverova <n.haverova@eedc.sk>
2022-01-07 09:06:24 +0100

    set up core infrastructure

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+
+        //We want 4 products per page
+        public int PageSize = 4;
+ 
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ViewResult List()
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                                     .Where(p => category == null || p.Category == category)
+                                     .OrderBy(p => p.ProductID)
+                                     .Skip((page - 1) * PageSize)
+                                     .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
+
+             /* return View(repository.Products
+                                      .OrderBy(p => p.ProductID)    //order by primary key
+                                      .Skip((page -1) * PageSize)   //skip over products occuring before the start current pg
+                                      .Take(PageSize));             //take only no. of specified products
+             */
+             
         }
     }
 }
