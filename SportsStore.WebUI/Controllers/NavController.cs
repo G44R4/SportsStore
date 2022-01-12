953176e4c1bd06e1a69975107c11047c05b8c79b
Nikoletta Haverova <n.haverova@eedc.sk>
2022-01-12 11:21:11 +0100

    responsivity added

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -17,11 +17,13 @@ namespace SportsStore.WebUI.Controllers
         public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category; //to highlight current category 
+
             IEnumerable<string> categories = repository.Products
                                                        .Select(x => x.Category)
                                                        .Distinct()
                                                        .OrderBy(x => x);
-            return PartialView(categories);
+
+            return PartialView("FlexMenu", categories);
         }
        
 
