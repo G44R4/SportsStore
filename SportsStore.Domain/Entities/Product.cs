289012140bda36a15843365a94a1e847d52896ac
Nikoletta Haverova <n.haverova@eedc.sk>
2022-01-17 09:40:03 +0100

    image upload functionality

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,16 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportsStore.Domain.Entities
 {
     [Table("Products", Schema = "public")]
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
-        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
+        public string Category { get; set; }
+
+        [Column("imagedata")]
+        public byte[] ImageData { get; set; }
+
+        [Column("imagemimetype")]
+        public string ImageMimeType { get; set; }
     }
 }
